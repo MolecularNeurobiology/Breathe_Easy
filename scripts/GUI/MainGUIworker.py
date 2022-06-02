1e784acd131add5a587f3d43a283599a95158c52
MolecularNeurobiology <95201519+MolecularNeurobiology@users.noreply.github.com>
2022-06-02 17:50:26 -0500

    update rscript name to current version
    Data_import.r (previously Data_import_multi.r)

--- scripts/GUI/MainGUIworker.py
+++ scripts/GUI/MainGUIworker.py
@@ -152,7 +152,7 @@ def get_jobs_r(rscript, pipeline, papr_dir, workspace_dir, input_dir_r, variable
         # The path to the directory for STAGG output
         '-O', output_dir,
         # The paths to the STAGG scripts
-        '-T', os.path.join(papr_dir, "Data_import_multi.R"),
+        '-T', os.path.join(papr_dir, "Data_import.R"),
         '-S', os.path.join(papr_dir, "Statistical_analysis.R"),
         '-M', os.path.join(papr_dir, "Graph_generator.R"),
         '-B', os.path.join(papr_dir, "Optional_graphs.R"),
