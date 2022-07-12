251ae1c20980143bb35faae82fa9e0e6c9632403
Avery <atwitchellheyne@gmail.com>
2022-07-12 16:55:48 -0500

    Issue #140. Fixed max vevo2 reference connection hopefully in sbasic subGUI.

--- scripts/GUI/config.py
+++ scripts/GUI/config.py
@@ -1348,7 +1348,8 @@ class VariableSettings(ConfigSettings):
             trans = [t.replace("raw", "non") for t in trans]
             trans = [t.replace("ln", "log") for t in trans]
             df.at[i, 'Transformation'] = '@'.join(trans)
-
+        print(trans)
+        print(df)
         df.to_csv(filepath, index=False)
 
 class GraphSettings(ConfigSettings):
