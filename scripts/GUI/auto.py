42830b55191afe62341ecaf4b8c9e34f1015310d
Christopher S Ward <34755351+realchrisward@users.noreply.github.com>
2022-10-05 12:54:26 -0500

    (C) info added

--- scripts/GUI/auto.py
+++ scripts/GUI/auto.py
@@ -1,6 +1,27 @@
 """
 Module for the autosections GUI, allowing the user to select
 options for an automated run of BASSPRO.
+
+***
+built as part of the Russell Ray Lab Breathing And Physiology Analysis Pipeline
+***
+Breathe Easy - an automated waveform analysis pipeline
+Copyright (C) 2022  
+Savannah Lusk, Andersen Chang, 
+Avery Twitchell-Heyne, Shaun Fattig, 
+Christopher Scott Ward, Russell Ray.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***
+
 """
 from typing import List, Optional
 import numpy as np
