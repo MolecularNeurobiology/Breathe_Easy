7cc69ef24445fd86b2736cb4b45399713cddf5ae
Chris Ward <ward.chris.s@gmail.com>
2022-10-06 13:17:42 -0500

    fixed typo - header docstring collided with import statements

--- scripts/GUI/thinbass_controller.py
+++ scripts/GUI/thinbass_controller.py
@@ -20,7 +20,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ***
 
-"""from typing import List
+"""
+from typing import List
 from PyQt5.QtWidgets import QDialog, QPushButton
 from PyQt5.QtCore import Qt
 from ui.thinbass import Ui_Thinbass
