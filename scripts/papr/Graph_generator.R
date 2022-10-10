2fd34865c17cae769587dec3bc09e062ba542551
Andersen <atc7@rice.edu>
2022-10-10 15:55:43 -0500

    Updated comments in STAGG

--- scripts/papr/Graph_generator.R
+++ scripts/papr/Graph_generator.R
@@ -81,6 +81,7 @@ graph_reorder <- function(graph_data_frame, plot_vars, graph_config, full_data){
 ### facet1: character string, name of independent variable mapped to row-wise faceting.
 ### facet2: character string, name of independent variable mapped to column-wise faceting.
 ### graph_data: data frame, mouse-wise averaged data to be graphed.
+### run_data: data frame, raw data.
 ### tukey_res: data frame, multiple comparisons table output from statistical analysis.
 ### inter_vars: character vector, names of independent variables.
 ### savename: character string, name of graph file when saved,
